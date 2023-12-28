6e6fda7e27f1d6564d15598d91848753ce6ec80f
chalankakodikara <ischalankakodikara@gmail.com>
2023-12-28 10:01:41 +0530

    Backend not set up

--- login/src/App.js
+++ login/src/App.js
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<Signup />} />
           <Route path="/login" element={<Login />} />
           
         </Routes>
